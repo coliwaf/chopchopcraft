5f182b26c9dea0a924c24fe5335032a9ef37d6cb
Collins Sande <coliwafula@gmail.com>
2026-07-26 03:20:53 +0300

    Cleanup Providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,8 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+
+    ...(app()->environment('local') ? [
+        App\Providers\TelescopeServiceProvider::class,
+    ] : []),
 ];
